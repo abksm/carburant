ab0ba83153505249ddb28786d297ab81faceb685
Yohann <yzpt.de@gmail.com>
2023-11-05 17:57:23 +0100

    update journal

---
+++ journal.sh
@@ -0,0 +1,17 @@
+# git new branch cmd
+git checkout -b yz
+git add .
+git commit -m "add journal.sh"
+git push origin yz
+
+git checkout main
+git config pull.rebase false
+git pull origin main
+
+# git delete branch
+git branch -d yz
+git push origin --delete yz
+
+# display origin url
+git remote show origin
+
