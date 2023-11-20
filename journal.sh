e7f77c5e1ffc8b569eccef2ad5f11f3ce2f8fe41
Yohann <yzpt.de@gmail.com>
2023-11-20 20:43:48 +0100

    -

--- journal.sh
+++ journal.sh
@@ -19,7 +19,6 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install requests
-pip install "apache-airflow[celery]==2.7.2" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.7.2/constraints-3.10.txt"
 pip install google-cloud-storage
 pip install google-cloud-bigquery
 
@@ -55,12 +54,30 @@ gcloud billing projects link $PROJECT_ID --billing-account=$BILLING_ACCOUNT_ID
 
 
 
+# =====================   AIRFLOW   ========================================================================
+# https://www.youtube.com/watch?v=K9AnJ9_ZAXE&t=2142s
+
+# Airflow py_env ===========================================================================================
+pip install "apache-airflow[celery]==2.7.2" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.7.2/constraints-3.10.txt"
+# set env variable
+export AIRFLOW_HOME=/home/yzpt/projects/carburant_gcp
+airflow db init
+airflow users create --username admin --firstname Peter --lastname Parker --role Admin --email allo@allo.com
+airflow webserver --port 8080
+
+# new term:
+export AIRFLOW_HOME=/home/yzpt/projects/carburant_gcp
+airflow scheduler
 
 
 
 
-# Airflow Docker :
+
+# Airflow Docker ===========================================================================================
+# https://airflow.apache.org/docs/docker-stack/index.html
 curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.7.3/docker-compose.yaml'
 mkdir ./dags ./logs ./plugins
 echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
 docker compose up airflow-init
+docker compose down --volumes --remove-orphans
+docker compose up
